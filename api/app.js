54e457389878dfecfa034331abd04c2e9c2a61b2
Josh Lee <joshlee.dev@gmail.com>
2021-09-08 17:30:31 +0100

    implement api redirect

--- api/app.js
+++ api/app.js
@@ -5,29 +5,40 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 const port = process.env.PORT || 8080;
-const siteDomain = 'localhost:3000/'
+const siteDomain = 'localhost:8080/'
 console.log(`default endpoint: localhost:${port}`);
 
 //in memory db
 const linkDB = [];
 
+//create new short link
 app.post('/create-link', (req, res) => {
     const userLink = req.body.userLink;
     // isValidLink(userLink);
-    const genLink = `${siteDomain}${generateShortLink()}`;
+    const genLink = `${generateShortLink()}`;
     const linkPair = {
         longLink: userLink,
         shortLink: genLink
     };
     linkDB.push(linkPair);
-    res.send(genLink);
+    res.send(`${siteDomain}${genLink}`);
 });
 
+//redirect from shortlink
+app.get('/:linkID', (req, res) => {
+    const linkPair = linkDB.find(linkPair => linkPair.shortLink === req.params.linkID);
+    console.log('linkPair: ', linkPair);
+    console.log('linkID: ', req.params.linkID);
+    console.log('linkDB: ', linkDB);
+    if(!linkPair) return res.status(404).send('link not found.');
+    res.redirect(linkPair.longLink); 
+});
 
 //generate unique code
 const generateShortLink = () => {
-    return (linkDB.length + 1).toString();
+    return linkDB.length + 1;
 }
 
 
@@ -42,12 +53,7 @@ const isValidLink = (url) => {
     }
 }
 
-//get longlink from shortlink
-// app.get('/:shortlink', (req, res) => {
-//     const linkPair = linkDB.find(linkPair => linkPair.shortLink === parseInt(req.params.shortlink));
-//     if(!linkPair) return res.status(404).send('link not found.');
-//     res.redirect(linkPair.longLink); 
-// });
+
 
 
 app.listen(port);
