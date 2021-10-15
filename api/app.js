7f1b7eb5fd7ceb2a1b23cce1609671d6be0c9c91
Josh Lee <joshlee.dev@gmail.com>
2021-10-15 14:17:39 +0100

    cleaned up and fixed error handling - query errors handled within callback

--- api/app.js
+++ api/app.js
@@ -4,11 +4,12 @@ const shortid = require('shortid');
 const axios = require('axios');
 const mysql = require('mysql');
 
+//API Setup
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-//api domain/port definitions
+//API domain/port definitions
 const port = process.env.PORT || 8080;
 const siteDomain = 'localhost:8080/'
 console.log(`default endpoint: localhost:${port}`);
@@ -41,22 +42,25 @@ con.query('USE urlshortener');
 
 //handle create new link request
 app.post('/create-link', async (req, res) => {
-    const userLink = req.body.userLink;
-    
+    const userLink = req.body.userLink; //store user provided link
+
     try {
         //user link validation
         validateLink(userLink);
         const feedback = await checkIfActive(userLink);
-        
+
         const genLink = generateShortLink(); //create unique shortlink
         
         //DB query to create new entry,
         const sql = 'INSERT INTO links SET ?';
         let values = createURLPair(genLink, userLink);
         con.query(sql, values, (err) => {
-            if(err) throw err;
+            if(err) {
+                console.log(err);
+                res.status(500).send('Server Error: Bad Query');
+            }
+            else res.status(200).json({ result: `${siteDomain}${genLink}`, feedback }); //response to client
         });
-        res.status(200).json({ result: `${siteDomain}${genLink}`, feedback }); //response to client
 
     } catch (error) {
         console.log('error: ', error); //api error log
@@ -67,23 +71,27 @@ app.post('/create-link', async (req, res) => {
 //handle redirect from shortlink
 app.get('/:linkID', async (req, res) => {
 
-    //DB query to retieve corresponding longlink
-    const sql = 'SELECT long_link FROM links WHERE short_link = ?';
-    let url = escapeParam(req.params.linkID);
+    try {
+        //DB query to retieve corresponding longlink
+        const sql = 'SELECT long_link FROM links WHERE short_link = ?';
+        let url = escapeParam(req.params.linkID);
 
-    //query for shortlink match, returning shortlink
-   con.query(sql, url, (err, rows) => {
-        try {
-            if(rows[0].hasOwnProperty('long_link')){ //check db response includes full link pair
-                url = unescapeParam(rows[0].long_link)
-                res.redirect(url);
+        //query for shortlink match, returning shortlink
+        con.query(sql, url, (err, rows) => {
+            if(err) {
+                console.log(err);
+                return res.status(500).send('Server Error: Bad Query');
             }
+            if(!rows[0].hasOwnProperty('long_link')){ //check db response includes full link pair
+                return res.status(404).send('Link Not Found');
+            }
+            url = unescapeParam(rows[0].long_link);
+            res.redirect(url);
+        });
 
-        } catch (error) {
-            console.log(error);
-            return res.status(404).send('link not found.')
-        }
-    });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
@@ -98,7 +106,10 @@ const generateShortLink = () => {
     //query for all shortlinks in DB and store in array
     let shortLinks = [];
     con.query('SELECT short_link FROM links', (err, rows) => {
-        if(err) throw err;
+        if(err) {
+            console.log(err);
+            return res.status(500).send('Server Error: Bad Query');
+        }
         rows.forEach((row) => {
             shortLinks.push(row.short_link);
         })
@@ -112,7 +123,7 @@ const generateShortLink = () => {
         }
         code = shortid.generate();
     }
-    throw new Error('failed to generate code');
+    throw 'failed to generate code';
 }
 
 //create url pair from user provided link(longlink) and generated code link(shortlink)
